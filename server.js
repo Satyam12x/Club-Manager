bfadfdfae5830b2b00ee7db408f06e1a92cbb563
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-26 22:05:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1974,5 +1974,7 @@ app.get("/api/attendance", authenticateToken, isSuperAdminOrAdmin, async (req, r
   }
 });
 
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
